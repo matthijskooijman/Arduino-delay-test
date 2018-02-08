29b4c817c10ed278ef46db12ea7d9d501a07d951
Matthijs Kooijman <matthijs@stdin.nl>
2018-02-08 14:28:22 +0100

    Initial test framework
    This has an empty delayMicroseconds implementation.

---
+++ DelayTest.ino
@@ -0,0 +1,105 @@
+struct Results {
+  uint32_t f_cpu;
+  uint16_t ok_count;
+  uint16_t error_count;
+  uint32_t total_error;
+};
+Results results[7];
+
+static void process_results(Results& results, uint16_t us, uint16_t start, uint16_t end, uint8_t cpu_mhz) {
+  uint16_t cycles = (end - start);
+  // Subtract the time to read TCNT1: 2 lds instructions at 2 cycles each
+  cycles -= 4;
+  Serial.print("delayMicroseconds(");
+  Serial.print(us);
+  Serial.print("): ");
+  uint16_t expected_cycles = us * cpu_mhz;
+  if (expected_cycles == cycles) {
+    Serial.println("OK");
+    results.ok_count++;
+  } else {
+    Serial.print("ERR: ");
+    int16_t err_cycles = cycles - expected_cycles;
+    Serial.print(err_cycles);
+    Serial.print(" cycles == ");
+    Serial.print(err_cycles / float(cpu_mhz));
+    Serial.println("us");
+
+    results.error_count++;
+    results.total_error += abs(err_cycles);
+  }
+}
+
+namespace CPU24 {
+  #undef F_CPU
+  #define F_CPU 24000000
+  #include "test.h"
+}
+namespace CPU20 {
+  #undef F_CPU
+  #define F_CPU 20000000
+  #include "test.h"
+}
+namespace CPU16 {
+  #undef F_CPU
+  #define F_CPU 16000000
+  #include "test.h"
+}
+namespace CPU8 {
+  #undef F_CPU
+  #define F_CPU 8000000
+  #include "test.h"
+}
+namespace CPU4 {
+  #undef F_CPU
+  #define F_CPU 4000000
+  #include "test.h"
+}
+namespace CPU2 {
+  #undef F_CPU
+  #define F_CPU 2000000
+  #include "test.h"
+}
+namespace CPU1 {
+  #undef F_CPU
+  #define F_CPU 1000000
+  #include "test.h"
+}
+
+//void measurerange(const uint16_t from, const uint16_t to, const uint16_t step = 1) __attribute__((__always_inline__));
+
+void setup() {
+  Serial.begin(115200);
+  // Start timer1, run with /1 prescaler
+  TCCR1A = 0;
+  TCCR1B = (1 << CS10);
+
+  // Disable interrupts, so we don't measure interference from the timer or serial interrupts
+  cli();
+  CPU24::measure_all(results[0]);
+  CPU20::measure_all(results[1]);
+  CPU16::measure_all(results[2]);
+  CPU8::measure_all(results[3]);
+  CPU4::measure_all(results[4]);
+  CPU2::measure_all(results[5]);
+  CPU1::measure_all(results[6]);
+  sei();
+  
+  Serial.println();
+  Serial.println(">> Summary");
+  for (auto result : results) {
+    if (!result.f_cpu)
+      continue;
+    Serial.print(result.f_cpu / 1000000);
+    Serial.print("Mhz: ");
+    Serial.print(result.ok_count);
+    Serial.print(" ok, ");
+    Serial.print(result.error_count);
+    Serial.print(" errors, totalling ");
+    Serial.print(result.total_error);
+    Serial.println(" cycles");
+  }
+}
+
+void loop() {
+}
---
+++ test.h
@@ -0,0 +1,286 @@
+void delayMicroseconds(uint16_t usec)
+{
+}
+
+// This needs always_inline to ensure any constant argument reaches delayMicroseconds
+static inline void measure_one(Results&, uint16_t) __attribute__((__always_inline__));
+static inline void measure_one(Results& results, uint16_t us) {
+  uint16_t start, end;
+  // Access TCNT1 using assembly, so we can guarantee to know the exact overhead
+  asm volatile (
+    "lds %A0, %1\n"
+    "lds %B0, %2\n"
+    : "=w" (start)
+    : "M" (_SFR_MEM_ADDR(TCNT1L)),
+      "M" (_SFR_MEM_ADDR(TCNT1H))
+  );
+  
+  delayMicroseconds(us);
+  
+  asm volatile (
+    "lds %A0, %1\n"
+    "lds %B0, %2\n"
+    : "=w" (end)
+    : "M" (_SFR_MEM_ADDR(TCNT1L)),
+      "M" (_SFR_MEM_ADDR(TCNT1H))
+  );
+  process_results(results, us, start, end, F_CPU / 1000000);
+}
+
+static void measure_range(Results& results, uint16_t from, const uint16_t to, const uint16_t step = 1) {
+  while (from <= to) {
+    measure_one(results, from);
+    from += step;
+  }
+}
+
+static void measure_all(Results& results) {
+  Serial.print(">> Constants @");
+  Serial.print(F_CPU / 1000000);
+  Serial.println("Mhz");
+  
+  // Generated using bash:
+  // for i in {0..100}; do echo "measure_one(results, $i);"; done
+  // for i in {101..1000..7}; do echo "measure_one(results, $i);"; done
+  // To save time and space, does not test all values, but instead skips a prime
+  // number every time, hoping to at least test some corner cases too.
+  measure_one(results, 0);
+  measure_one(results, 1);
+  measure_one(results, 2);
+  measure_one(results, 3);
+  measure_one(results, 4);
+  measure_one(results, 5);
+  measure_one(results, 6);
+  measure_one(results, 7);
+  measure_one(results, 8);
+  measure_one(results, 9);
+  measure_one(results, 10);
+  measure_one(results, 11);
+  measure_one(results, 12);
+  measure_one(results, 13);
+  measure_one(results, 14);
+  measure_one(results, 15);
+  measure_one(results, 16);
+  measure_one(results, 17);
+  measure_one(results, 18);
+  measure_one(results, 19);
+  measure_one(results, 20);
+  measure_one(results, 21);
+  measure_one(results, 22);
+  measure_one(results, 23);
+  measure_one(results, 24);
+  measure_one(results, 25);
+  measure_one(results, 26);
+  measure_one(results, 27);
+  measure_one(results, 28);
+  measure_one(results, 29);
+  measure_one(results, 30);
+  measure_one(results, 31);
+  measure_one(results, 32);
+  measure_one(results, 33);
+  measure_one(results, 34);
+  measure_one(results, 35);
+  measure_one(results, 36);
+  measure_one(results, 37);
+  measure_one(results, 38);
+  measure_one(results, 39);
+  measure_one(results, 40);
+  measure_one(results, 41);
+  measure_one(results, 42);
+  measure_one(results, 43);
+  measure_one(results, 44);
+  measure_one(results, 45);
+  measure_one(results, 46);
+  measure_one(results, 47);
+  measure_one(results, 48);
+  measure_one(results, 49);
+  measure_one(results, 50);
+  measure_one(results, 51);
+  measure_one(results, 52);
+  measure_one(results, 53);
+  measure_one(results, 54);
+  measure_one(results, 55);
+  measure_one(results, 56);
+  measure_one(results, 57);
+  measure_one(results, 58);
+  measure_one(results, 59);
+  measure_one(results, 60);
+  measure_one(results, 61);
+  measure_one(results, 62);
+  measure_one(results, 63);
+  measure_one(results, 64);
+  measure_one(results, 65);
+  measure_one(results, 66);
+  measure_one(results, 67);
+  measure_one(results, 68);
+  measure_one(results, 69);
+  measure_one(results, 70);
+  measure_one(results, 71);
+  measure_one(results, 72);
+  measure_one(results, 73);
+  measure_one(results, 74);
+  measure_one(results, 75);
+  measure_one(results, 76);
+  measure_one(results, 77);
+  measure_one(results, 78);
+  measure_one(results, 79);
+  measure_one(results, 80);
+  measure_one(results, 81);
+  measure_one(results, 82);
+  measure_one(results, 83);
+  measure_one(results, 84);
+  measure_one(results, 85);
+  measure_one(results, 86);
+  measure_one(results, 87);
+  measure_one(results, 88);
+  measure_one(results, 89);
+  measure_one(results, 90);
+  measure_one(results, 91);
+  measure_one(results, 92);
+  measure_one(results, 93);
+  measure_one(results, 94);
+  measure_one(results, 95);
+  measure_one(results, 96);
+  measure_one(results, 97);
+  measure_one(results, 98);
+  measure_one(results, 99);
+  measure_one(results, 100);
+  measure_one(results, 101);
+  measure_one(results, 108);
+  measure_one(results, 115);
+  measure_one(results, 122);
+  measure_one(results, 129);
+  measure_one(results, 136);
+  measure_one(results, 143);
+  measure_one(results, 150);
+  measure_one(results, 157);
+  measure_one(results, 164);
+  measure_one(results, 171);
+  measure_one(results, 178);
+  measure_one(results, 185);
+  measure_one(results, 192);
+  measure_one(results, 199);
+  measure_one(results, 206);
+  measure_one(results, 213);
+  measure_one(results, 220);
+  measure_one(results, 227);
+  measure_one(results, 234);
+  measure_one(results, 241);
+  measure_one(results, 248);
+  measure_one(results, 255);
+  measure_one(results, 262);
+  measure_one(results, 269);
+  measure_one(results, 276);
+  measure_one(results, 283);
+  measure_one(results, 290);
+  measure_one(results, 297);
+  measure_one(results, 304);
+  measure_one(results, 311);
+  measure_one(results, 318);
+  measure_one(results, 325);
+  measure_one(results, 332);
+  measure_one(results, 339);
+  measure_one(results, 346);
+  measure_one(results, 353);
+  measure_one(results, 360);
+  measure_one(results, 367);
+  measure_one(results, 374);
+  measure_one(results, 381);
+  measure_one(results, 388);
+  measure_one(results, 395);
+  measure_one(results, 402);
+  measure_one(results, 409);
+  measure_one(results, 416);
+  measure_one(results, 423);
+  measure_one(results, 430);
+  measure_one(results, 437);
+  measure_one(results, 444);
+  measure_one(results, 451);
+  measure_one(results, 458);
+  measure_one(results, 465);
+  measure_one(results, 472);
+  measure_one(results, 479);
+  measure_one(results, 486);
+  measure_one(results, 493);
+  measure_one(results, 500);
+  measure_one(results, 507);
+  measure_one(results, 514);
+  measure_one(results, 521);
+  measure_one(results, 528);
+  measure_one(results, 535);
+  measure_one(results, 542);
+  measure_one(results, 549);
+  measure_one(results, 556);
+  measure_one(results, 563);
+  measure_one(results, 570);
+  measure_one(results, 577);
+  measure_one(results, 584);
+  measure_one(results, 591);
+  measure_one(results, 598);
+  measure_one(results, 605);
+  measure_one(results, 612);
+  measure_one(results, 619);
+  measure_one(results, 626);
+  measure_one(results, 633);
+  measure_one(results, 640);
+  measure_one(results, 647);
+  measure_one(results, 654);
+  measure_one(results, 661);
+  measure_one(results, 668);
+  measure_one(results, 675);
+  measure_one(results, 682);
+  measure_one(results, 689);
+  measure_one(results, 696);
+  measure_one(results, 703);
+  measure_one(results, 710);
+  measure_one(results, 717);
+  measure_one(results, 724);
+  measure_one(results, 731);
+  measure_one(results, 738);
+  measure_one(results, 745);
+  measure_one(results, 752);
+  measure_one(results, 759);
+  measure_one(results, 766);
+  measure_one(results, 773);
+  measure_one(results, 780);
+  measure_one(results, 787);
+  measure_one(results, 794);
+  measure_one(results, 801);
+  measure_one(results, 808);
+  measure_one(results, 815);
+  measure_one(results, 822);
+  measure_one(results, 829);
+  measure_one(results, 836);
+  measure_one(results, 843);
+  measure_one(results, 850);
+  measure_one(results, 857);
+  measure_one(results, 864);
+  measure_one(results, 871);
+  measure_one(results, 878);
+  measure_one(results, 885);
+  measure_one(results, 892);
+  measure_one(results, 899);
+  measure_one(results, 906);
+  measure_one(results, 913);
+  measure_one(results, 920);
+  measure_one(results, 927);
+  measure_one(results, 934);
+  measure_one(results, 941);
+  measure_one(results, 948);
+  measure_one(results, 955);
+  measure_one(results, 962);
+  measure_one(results, 969);
+  measure_one(results, 976);
+  measure_one(results, 983);
+  measure_one(results, 990);
+  measure_one(results, 997);
+  
+  Serial.print(">> Variables @");
+  Serial.print(F_CPU / 1000000);
+  Serial.println("Mhz");
+  measure_range(results, 0, 100);
+  measure_range(results, 101, 1000, 7);
+
+  results.f_cpu = F_CPU;
+}
+
